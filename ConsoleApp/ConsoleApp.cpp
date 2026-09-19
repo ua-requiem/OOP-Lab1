ce7d48a59d033cf71255602834133a05461c35df
Datsenko Evhenii <uarequiem27@gmail.com>
2026-09-19 16:02:12 +0300

    Calculation of the sum of numbers and output of the result have been added

--- ConsoleApp/ConsoleApp.cpp
+++ ConsoleApp/ConsoleApp.cpp
@@ -1,2 +1,13 @@
-﻿Console.WriteLine("Hello world!");
-Console.ReadKey();
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 10;
+            Console.WriteLine("5 + 10 = {0}", x + y);
+            Console.ReadKey();
+        }
+    }
+}
